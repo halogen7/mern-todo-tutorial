05917def1fce2fff1b4a7bb8a76463edb6e9a3f9
Mars <gmar.gilb@gmail.com>
2021-09-09 17:01:12 -0400

    Updates model and routes to track completed status

--- models/todos.js
+++ models/todos.js
@@ -6,6 +6,10 @@ const TodoSchema = new Schema({
     type: String,
     required: [true, "The todo text field is required"],
   },
+  completed: {
+    type: Boolean,
+    required: [true, "The todo completed field is required"]
+  }
 });
 
 const Todo = mongoose.model("todo", TodoSchema);
--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const Todo = require("../models/todos");
 const router = express.Router();
 
 router.get("/todos", (req, res, next) => {
-  Todo.find({}, "action")
+  Todo.find({})
     .then((data) => res.json(data))
     .catch(next);
 });
@@ -15,6 +15,16 @@ router.post("/todos", (req, res, next) => {
       .catch(next);
   } else {
     res.json({ error: "The input field is empty" });
+  }
+});
+
+router.post("/todos/:id", (req, res, next) => {
+  if (req.body.completed) {
+    Todo.findByIdAndUpdate({_id:req.params.id}, {completed: req.body.completed}, {returnOriginal: false})
+      .then((data) => res.json(data))
+      .catch(next);
+  } else {
+    res.json({ error: "The completed field is empty" });
   }
 });
 
