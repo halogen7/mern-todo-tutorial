e950fcb162472df85d1946933182b7e56eaa5037
Mars <gmar.gilb@gmail.com>
2021-09-08 20:44:58 -0400

    Updates css; app entry point; imports font

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
-import React from "react";
 import Todo from "./components/Todo";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
       <Todo />
-    </div>
   );
 }
 
